0a0e4822b3533f46e9151d75e0d0fd0c6acf3d7b
Jon Rogozen <jon.rogozen@gmail.com>
2014-07-21 00:06:19 -0700

    finish racecar.rb

--- race.rb
+++ race.rb
@@ -1,5 +1,88 @@
-class RaceCars
+class RaceCar
+	attr_accessor :name, :speed, :distance
+	def initialize(name)
+		@name = name
+		@speed = 0
+		@distance = 0
+		puts "You have initialized a new Race Car, #{name}."
+	end
 end
 
-class RaceTracks
+class RaceTrack
+	attr_accessor :name, :race_time
+	def initialize(name)
+		@name = name
+		@race_cars = []
+		@race_time = 0
+		puts "You have initialized a new Race Track, #{name}."
+	end
+	def add_car(car)
+		@race_cars << car
+		puts "You have added #{car.name} to the race!"
+	end
+	def reset_race
+		#reset everyone to zero
+		@race_time = 0
+		@race_cars.each do |car|
+			car.speed = 0
+			car.distance = 0
+		end
+	end
+	def race(fast_forward = 1)
+		reset_race
+		if fast_forward <= 5
+			fast_forward.times do
+				@race_time += 1
+				if @race_time > 1
+					puts "The race has begun!"
+					puts "AFTER #{@race_time} HOURS OF RACING:"
+				else
+					puts "AFTER #{@race_time} HOUR OF RACING:"
+				end
+				@race_cars.each do |car|
+					car.speed = rand(60..80)
+					if @race_time > 1
+						car.speed += rand(0..20)
+					end
+					car.distance += car.speed
+					puts "#{car.name} is going #{car.speed} MPH."
+					puts "#{car.name} has traveled #{car.distance} miles."
+				end
+			end
+		else
+			puts "Races are only 5 hours, please retry."
+		end
+	end
+	def add_hour(hour=1)
+		if @race_time < 5 && @race_time + hour < 5
+			hour.times do
+				@race_time +=1 
+				puts "AFTER #{@race_time} HOURS OF RACING:"
+				@race_cars.each do |car|
+					car.speed += rand(0..20)
+					car.distance += car.speed
+					puts "#{car.name} is going #{car.speed} MPH."
+					puts "#{car.name} has traveled #{car.distance} miles."
+				end
+			end
+		else
+			puts "Sorry, races only last 5 hours. Please retry."
+		end
+	end
+	def determine_winner
+		cars_with_distance = {}
+		@race_cars.each do |car|
+			cars_with_distance[car.name] = car.distance
+		end
+		max_distance = 0
+		winner = ""
+		cars_with_distance.each do |car, distance|
+			if distance > max_distance
+				max_distance = distance
+				winner = car
+			end
+		end
+		p cars_with_distance
+		puts "The winner #{winner} traveled #{max_distance} miles in #{@race_time} hours."
+	end
 end
--- racetest.rb
+++ racetest.rb
@@ -1,1 +1,25 @@
 require_relative 'race'
+
+blue_car = RaceCar.new("Blue Car")
+red_car = RaceCar.new("Red Car")
+
+fast_track = RaceTrack.new("Fast Race Track")
+
+fast_track.add_car(blue_car)
+fast_track.add_car(red_car)
+
+#should fail
+fast_track.race(10)
+fast_track.race(1)
+fast_track.add_hour(10)
+
+fast_track.race(5)
+fast_track.determine_winner
+
+fast_track.race
+fast_track.add_hour(1)
+fast_track.add_hour(1)
+fast_track.add_hour(1)
+fast_track.add_hour(1)
+fast_track.add_hour(1) #fail
+fast_track.determine_winner
